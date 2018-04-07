cfc458f6c9c5fbbada520b19c06d5f304bcca9ce
Мика <demlusik@mail.ru>
2018-04-07 23:23:25 +0700

    done

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -8,38 +8,54 @@
 #include <string>
 using namespace std;
 
+vector <City> cityVec;
+
+string getNearest(double lat, double lon)
+{
+	double distance;
+	double nearest = 100000;
+	string city;
+	for each (City c in cityVec)
+	{
+		distance = sqrt(pow(lat - c.latitude, 2) + pow(lon - c.longitude, 2));
+		if (distance < nearest) {
+			nearest = distance;
+			city = c.name;
+		}
+	}
+	return city;
+}
+
 int main()
 {
-	vector <City> cityVec;
+	
 	//setlocale(LC_ALL, "rus"); // корректное отображение Кириллицы
 	char buff[100]; // буфер промежуточного хранения считываемого из файла текста
-	ifstream fin("c:\\Download\\data.csv"); // открыли файл для чтения
+	ifstream fin("C:\\Download\\data.csv"); // открыли файл для чтения
 	fin.getline(buff, 100); //первая строка нас не интересует
 	int i = 0; //счечик
 	string name = "";
-	float latitude = 0, longitud = 0;
-
-	while (!fin.eof())
+	double latitude = 0, longitude = 0;
+	int j = 0;
+//	while (!fin.eof())
+	while (j < 5)
 	{
+		++j;
 		City cit;
 		fin.getline(buff, 100); // считали строку из файла
 		char * pch = strtok(buff, ","); // замутили разделители
 		i = 0;
 		while (pch != NULL)                         // пока есть лексемы
 		{
-			
-			char* s = "34.32";
-			double f = atof(s);
-
 			if (i == 0) name = pch;
 			if (i == 2) latitude = atof(pch);
-			if (i == 3) longitud = atof(pch);
+			if (i == 3) longitude = atof(pch);
 			if (i > 3) break;
 			pch = strtok(NULL, ",");
 			++i;
 		}
 
-		cit.setCity(name, latitude, longitud);
+		cit.setCity(name, latitude, longitude);
 		cityVec.push_back(cit);
 	}
 	fin.close(); // закрываем файл
@@ -47,8 +63,20 @@ int main()
 	Distance distance;
 	distance.buildMatrix(cityVec);
 	distance.showMatrix();
+	
+	double lat = 36.9316;
+	double lon = 65.101;
+	cout << "---------------------------------------------------------------------------" << endl << endl;
+	cout << "The nearest city to " << lat << ", " << lon << " is " << getNearest(lat, lon)<< endl;
+
+	cout << "---------------------------------------------------------------------------" << endl << endl;
+	cout << "mean = " << distance.getMean() << endl;
+
+	cout << "---------------------------------------------------------------------------" << endl << endl;
+	cout << "dispersion = " << distance.getDispersion() << endl;
 
 	system("pause");
 	return 0;
 
 }
+
--- ConsoleApplication1/ConsoleApplication1/Distance.cpp
+++ ConsoleApplication1/ConsoleApplication1/Distance.cpp
@@ -1,21 +1,36 @@
 #include "stdafx.h"
 #include "Distance.h"
-#include "Distance.h"
 #include <vector>
 #include "City.h"
-#define sqr(x) x*x
 
 
+Distance::Distance()
+{
+	
+
+	for (int i = 0; i < 5; i++)
+	{
+		vector<double> row;
+		for (int j = 0; j < 5; j++)
+		{
+			row.push_back(0);
+		}
+		distanceVec.push_back(row);
+	}
+
+
+};
 void Distance::buildMatrix(vector <City> cityVec)
 {
-	vector<City>::iterator itA = cityVec.begin();
-	vector<City>::iterator itB = cityVec.begin();
-	for (vector<vector<float >>::iterator it2 = distanceVec.begin(); it2 != distanceVec.end(); it2++) {
-		for (vector<float>::iterator it = (distanceVec.at(distance(distanceVec.begin(), it2))).begin(); it != (distanceVec.at(distance(distanceVec.begin(), it2)).end()); it++) {
-			*it = calculateDistance((*itA).latitude, (*itA).longitude, (*itB).latitude, (*itB).longitude);
-			itB++;
+	int i = 0;int j=0;
+
+	for (vector<City>::iterator itA = cityVec.begin(); itA != cityVec.end(); itA++) {
+		for (vector<City>::iterator itB = cityVec.begin(); itB != cityVec.end(); itB++) {
+			distanceVec[i][j]=calculateDistance((*itA).latitude, (*itA).longitude, (*itB).latitude, (*itB).longitude);
+			++j;
 		}
-		itA++;
+		++i;
+		j = 0;
 	}
 
 
@@ -24,17 +39,50 @@ void Distance::buildMatrix(vector <City> cityVec)
 
 void Distance::showMatrix()
 {
-	for (vector<vector<float >>::iterator it2 = distanceVec.begin(); it2 != distanceVec.end(); it2++) {
-		for (vector<float>::iterator it = (distanceVec.at(distance(distanceVec.begin(), it2))).begin(); it != (distanceVec.at(distance(distanceVec.begin(), it2)).end()); it++) {
+	for (vector<vector<double >>::iterator it2 = distanceVec.begin(); it2 != distanceVec.end(); it2++) {
+		for (vector<double>::iterator it = (distanceVec.at(distance(distanceVec.begin(), it2))).begin(); it != (distanceVec.at(distance(distanceVec.begin(), it2)).end()); it++) {
 			cout << *it << "   "; 
 		}
-		cout << "==================\n";
+		cout <<endl<< "==================\n";
 	}
 }
 
-float Distance::calculateDistance(float latitude1, float longitude1, float latitude2, float longitude2)
+double Distance::calculateDistance(double latitude1, double longitude1, double latitude2, double longitude2)
 {
-	float distance;
-	distance = sqrt(sqr(latitude1 - latitude2) + sqr(longitude1 - longitude2));
+	double distance;
+	distance = sqrt(pow(latitude1 - latitude2,2) + pow(longitude1 - longitude2,2));
 	return distance;
+}
+
+double Distance::getMean()
+{
+	double mean = 0;
+	double sum = 0;
+	int count = 0;
+	for (vector<vector<double >>::iterator it2 = distanceVec.begin(); it2 != distanceVec.end(); it2++) 
+		for (vector<double>::iterator it = (distanceVec.at(distance(distanceVec.begin(), it2))).begin(); it != (distanceVec.at(distance(distanceVec.begin(), it2)).end()); it++) {
+			if (*it != 0) {
+				++count;
+				sum += *it;
+			}
+		}
+	mean = sum / count;
+	return mean;
+}
+
+double Distance::getDispersion()
+{
+	double mean = getMean();
+	double dispersion = 0;
+	double sum = 0;
+	int count = 0;
+	for (vector<vector<double >>::iterator it2 = distanceVec.begin(); it2 != distanceVec.end(); it2++)
+		for (vector<double>::iterator it = (distanceVec.at(distance(distanceVec.begin(), it2))).begin(); it != (distanceVec.at(distance(distanceVec.begin(), it2)).end()); it++) {
+			if (*it != 0) {
+				++count;
+				sum += (*it - mean)*(*it - mean);
+			}
+		}
+	dispersion = sum / count;
+	return dispersion;
 }
